3f97ed653444b024a84e4d64b69bf8c0a5ea3927
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-10-01 21:18:06 -0500

    Conflict master

--- Experimentacion1/microservice_monitoring_core/flaskr/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app(__name__):
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['PROPAGATE_EXCEPTIONS'] = True
-    return app
